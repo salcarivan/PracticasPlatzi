5d34b2fd5c3c5fedc0a5e59fc1f9101f6e5a5ee9
salcarivan <ivancarrascal97@gmail.com>
2022-04-27 19:08:30 +0200

    fin clase

---
+++ python2/clase1.py
@@ -0,0 +1,16 @@
+numero=5
+print (type(numero))
+nombre="Juan"
+print (type(nombre))
+decimal= 5.2
+print (type(decimal))
+mensaje= """Esto es un mensaje
+con tres saltos
+de linea"""
+print(mensaje)
+numero1=7
+numero2=9
+if numero1>numero2:
+    print("EL NUMERO 1 ES MAYOR")
+else:
+    print ("EL NUMERO 2 ES MAYOR")
---
+++ python2/clase2.py
@@ -0,0 +1,13 @@
+def mensaje():
+    print("estamos aprendiendo Python")
+    print("estamos aprendiendo intrucciones basicas")
+    print("poco a poco iremos avanzando")
+mensaje()
+print("texto fuera de la funcion")
+mensaje()
+def suma(num1,num2):
+    resultado=num1+num2
+    return resultado
+print(suma (5,7))
+print(suma(2,3))
+print(suma(35,358))
