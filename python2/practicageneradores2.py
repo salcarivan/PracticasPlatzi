640424316759760460cef79573c2fcf6a9b9e08e
salcarivan <ivancarrascal97@gmail.com>
2022-05-03 18:04:23 +0200

    correcciones03/05

--- python2/practicageneradores2.py
+++ python2/practicageneradores2.py
@@ -1,7 +1,7 @@
 def devuelveciudades (*ciudades):
     for elemento in ciudades:
         #for subelemento in elemento:
-            yield from elemento
+            yield from  elemento
 ciudadesdevueltas=devuelveciudades("Madrid","Sevilla","Cádiz","Málaga","Huelva","Córdoba","Almería","Jaen","Granada","Barcelona", "Santiago","Londres","Roma")
 print(next(ciudadesdevueltas))
 print(next(ciudadesdevueltas))
